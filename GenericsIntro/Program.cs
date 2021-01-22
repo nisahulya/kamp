9869aef6a192791fb35931218a84b98cf9eb5345
nisahulya <nisahulya94@gmail.com>
2021-01-22 19:23:20 +0300

    20 Ocak Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ refout2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace refout2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Topla1(10,20);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+
+        public int Topla1(int sayi3, int sayi4)
+        {
+            int sayi3 = 30;
+            int sonuc = sayi3 + sayi4;
+            return sonuc;
+        }
+    }
+}
